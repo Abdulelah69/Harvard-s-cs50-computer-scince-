1958f2fee7f97331d87fccc64e7b5d756eeb57ee
Abdulelah.kartal <165378310+Abdulelah69@users.noreply.github.com>
2025-09-08 12:33:46 +0300

    Add files via upload

---
+++ week1/mario_more/mario.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+
+    do
+    {
+        height = get_int("height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++)
+    {
+
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int l = 0; l <= i; l++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
